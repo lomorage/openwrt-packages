0ea223e8b2bbe971d3cca44214b9332bb00470a0
Jeromy Fu <fuji246@gmail.com>
2021-10-26 22:38:37 -0700

    fix pkg delete issue

--- release_lomod.sh
+++ release_lomod.sh
@@ -15,9 +15,6 @@ for arch in bin/targets/*; do
         echo "Found $ipkfile"
         cp $ipkfile $target/
         cp configs/$archname.config .config
-        make CONFIG_PACKAGE_lomo-backend=m package/lomo-backend/clean
-        make CONFIG_PACKAGE_lomo-backend_nohf=m package/lomo-backend/clean
-        make CONFIG_PACKAGE_lomo-web=m package/lomo-web/clean
     done
 
     pushd $target
